1a89954de38b7d501e9ba80930cf7138b7f1e381
Lucifer <ajayveersinghpasricha15@gmail.com>
2020-08-16 00:40:29 +0530

    timer functions added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+// import { Router, Route } from "react-router";
+import "./App.css";
+// import TicTacToe from "./TicTacToe/tictactoe";
+class App extends Component {
+  render() {
+    return (
+      <div className="main-container">
+        <p>This message is from a class component</p>
+        <h1>Hello World</h1>
+        <Welcome name="User"></Welcome>
+        <p>Click on the links to navigate further:</p>
+        <ol>
+        
+        </ol>
+      </div>
+    );
+  }
+}
 
-function App() {
+function Welcome(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>This message is from a functional component</p>
+      <h1>Welcome, {props.name}</h1>
     </div>
   );
 }
